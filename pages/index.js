86d80ee21a85a5ad2fd26a17f904f8b856528610
YuanAsh <150578126+YuanAsh@users.noreply.github.com>
2023-11-12 15:46:07 +0630

    Add files via upload

---
+++ pages/index.js
@@ -0,0 +1,22 @@
+import Head from 'next/head'
+import Header from '@HnF/Header'
+import Footer from '@HnF/Footer'
+
+export default function Home() {
+  return (
+    <div className="container">
+      <Head>
+        <title>Next.js Starter!</title>
+        <link rel="icon" href="/favicon.ico" />
+      </Head>
+
+      <main>
+        <Header title="I Love You Ash <br> All those times together with you and Everythings we have done together <br> Those are my precious Memories and <br> I wanna create much more beautiful memories with you Ash. <br> The Source of my Happiness is You My Little Princess. <br> Let's Be Together Forever Ash " />
+        <p className="description">
+        </p>
+      </main>
+
+      <Footer />
+    </div>
+  )
+}

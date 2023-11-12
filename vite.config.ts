236dfc70c008677b54bd29edcebff0e547b9ef4d
YuanAsh <150578126+YuanAsh@users.noreply.github.com>
2023-11-12 14:25:28 +0630

    Delete vite.config.ts

--- vite.config.ts
+++
@@ -1,7 +0,0 @@
-import solid from "solid-start/vite";
-import netlify from "solid-start-netlify";
-import { defineConfig } from "vite";
-
-export default defineConfig({
-  plugins: [solid({ adapter: netlify() })],
-});
